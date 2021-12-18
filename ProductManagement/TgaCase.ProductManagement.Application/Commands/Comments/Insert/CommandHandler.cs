04aab7241d1ca85e7a51c62b9c85309b20bfe6cd
mehmet <mmt.altnll@gmail.com>
2021-12-19 01:12:58 +0300

    comments

---
+++ ProductManagement/TgaCase.ProductManagement.Application/Commands/Comments/Insert/CommandHandler.cs
@@ -0,0 +1,33 @@
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using TgaCase.ProductManagement.Domain;
+using TgaCase.SharedKernel.SeedWork.Repository;
+
+namespace TgaCase.ProductManagement.Application.Commands.Category.Insert
+{
+    public class CommandHandler : IRequestHandler<Command,bool>
+    {
+        private IUnitOfWorkFactory<IProductManagementDbContext> _unitOfWork;
+        public IMapper _mapper;
+        public CommandHandler(IUnitOfWorkFactory<IProductManagementDbContext> unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<bool> Handle(Command request, CancellationToken cancellationToken)
+        {
+            using (var uow = _unitOfWork.Create(true, true))
+            {
+                var insert = await uow.Context.MAIN.Category.InsertAsync(new Domain.Schemas.MAIN.CategoryAggregates.Category
+                {
+                    Name =request.Name,
+                    ParentId = request.ParentId,
+                    IsActive = request.IsActive
+                });
+                uow.CommitTransaction();
+                return true;
+            }
+        }
+    }
+}
